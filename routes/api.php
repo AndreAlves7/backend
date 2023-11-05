d060bb3b9a454d53ea1f4605665950e663081676
veroight <veroight@gmail.com>
2023-11-05 03:39:10 +0000

    Fix logout route in api.php

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('auth/login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->post(
-    'logout',
-    [AuthController::class, 'auth/logout']
+Route::middleware('auth:api')->post( 
+    'auth/logout',
+    [AuthController::class, 'logout']
 );
 
 // Route::apiResource('/vcard', 'App\Http\Controllers\VcardController');
